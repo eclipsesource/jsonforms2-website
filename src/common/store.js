85fb8042f499e8e690ef26c8deee50e5200baecd
Edgar Mueller <emueller@eclipsesource.com>
2018-01-29 11:07:04 +0100

    Fix initialization of store

--- src/common/store.js
+++ src/common/store.js
@@ -1,19 +1,29 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { JsonForms, jsonformsReducer } from '@jsonforms/core';
+import { Actions, JsonForms, jsonformsReducer } from '@jsonforms/core';
 
-export const createJsonFormsStore = ({ data, schema, uischema }) => createStore(
-  jsonformsReducer(),
-  {
-    jsonforms: {
-      common: {
-        data,
-        schema,
-        uischema,
-      },
-      renderers: JsonForms.renderers,
-      fields: JsonForms.fields,
-    }
-  },
-  applyMiddleware(thunk)
-);
+export const createJsonFormsStore = ({ data, schema, uischema }) => {
+  const store = createStore(
+    jsonformsReducer(),
+    {
+      jsonforms: {
+        common: {
+          data,
+          schema,
+          uischema,
+        },
+        renderers: JsonForms.renderers,
+        fields: JsonForms.fields,
+      }
+    },
+    applyMiddleware(thunk)
+  );
+  store.dispatch({
+    type: Actions.INIT,
+    data,
+    schema,
+    uischema
+  });
+  store.dispatch(Actions.validate());
+  return store;
+};
