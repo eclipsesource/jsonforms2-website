81a3ecf121acd0fb3cf774e2cc66a2d4d6c71985
Edgar Mueller <emueller@eclipsesource.com>
2019-01-09 10:52:03 +0100

    Add feature set matrix

--- src/pages/docs/RendererSets.js
+++ src/pages/docs/RendererSets.js
@@ -1,15 +1,238 @@
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
 import DocPage from "./DocPage";
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import TableHead from "@material-ui/core/TableHead/TableHead";
+import Table from "@material-ui/core/Table/Table";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+import TableRow from "@material-ui/core/TableRow/TableRow";
 
 const RendererSets = () => (
   <DocPage>
     <Typography variant='h1'>
       Renderer Sets
     </Typography>
+
     <Typography variant='body1'>
-      This section describes the two renderer sets which are available for react and some specifica about them.
+      This is an overview of all available renderer sets and what feature they support
     </Typography>
+
+    <Typography variant='h2'>
+      JSON Schema features
+    </Typography>
+    <Table>
+      <TableHead>
+        <TableCell>JSON Schema</TableCell>
+        <TableCell>Renderer</TableCell>
+        <TableCell>React Material</TableCell>
+        <TableCell>Angular Material</TableCell>
+        <TableCell>Ionic</TableCell>
+        <TableCell>React Vanilla</TableCell>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>boolean</TableCell>
+          <TableCell>Checkbox</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell>Toggle</TableCell>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>integer</TableCell>
+          <TableCell>Number</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported' />
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell>Text</TableCell>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>String</TableCell>
+          <TableCell>Text</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell>Textarea</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Enum</TableCell>
+          <TableCell>Combo</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell/>
+          <TableCell>Autocomplete</TableCell>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Date format</TableCell>
+          <TableCell>Date field</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Time format</TableCell>
+          <TableCell>Time field</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Datetime format</TableCell>
+          <TableCell>Datetime field</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Object</TableCell>
+          <TableCell>Vertical grid</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Array of primitives</TableCell>
+          <TableCell>List</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Array of objects</TableCell>
+          <TableCell>Table</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell>List with Detail</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>oneOf</TableCell>
+          <TableCell>Tabs</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>allOf</TableCell>
+          <TableCell>Tabs</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>anyOf</TableCell>
+          <TableCell>Tabs</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+          <TableCell className='renderer-sets__table__not-supported'/>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <Typography variant='h2'>
+      UI Schema features
+    </Typography>
+    <Table>
+      <TableHead>
+        <TableCell>UI Schema</TableCell>
+        <TableCell>Renderer</TableCell>
+        <TableCell>React Material</TableCell>
+        <TableCell>Angular Material</TableCell>
+        <TableCell>Ionic</TableCell>
+        <TableCell>React Vanilla</TableCell>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>Vertical Layout</TableCell>
+          <TableCell>Vertical Grid</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Horizontal Layout</TableCell>
+          <TableCell>Horizontal Grid</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Categorization</TableCell>
+          <TableCell>Tabs</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Group</TableCell>
+          <TableCell>Group</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+        <TableRow>
+          <TableCell>Label</TableCell>
+          <TableCell>Text</TableCell>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+          <TableCell className='renderer-sets__table__supported'/>
+        </TableRow>
+      </TableBody>
+    </Table>
+
     <Typography variant='h2'>
       Material Renderer Set
     </Typography>
@@ -17,6 +240,7 @@ const RendererSets = () => (
       The Material Renderer Set is based on <a href="https://material-ui.com/"className='link'>Material UI</a>.
       So if you want to implement your own renderers and use them with the Material Renderer Set we recommend to use Material UI.
     </Typography>
+
     <Typography variant='h2'>
       Vanilla Renderer Set
     </Typography>
