4530370fb8da8a541938c503f7d6aeb6b557e8e2
Stefan Dirix <sdirix@eclipsesource.com>
2020-12-03 09:16:40 +0100

    Update and rename get_next_version.sh to build_with_next.sh

--- build_with_next.sh
+++ build_with_next.sh
@@ -2,6 +2,6 @@
 
 export NEXTVERSION=$(curl --silent "https://api.github.com/repos/eclipsesource/jsonforms/releases" | grep '"tag_name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
 
-DOCZ_NEXTVERSION=$NEXTVERSION npm run build
+DOCZ_NEXTVERSION=$NEXTVERSION npm run build:docz
 
 exit 0
