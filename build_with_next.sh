0008f6bad31ab5d9744fe9ce29febe99d9ad2b0e
Stefan Dirix <sdirix@eclipsesource.com>
2020-12-03 10:14:44 +0100

    Update build_with_next.sh

--- build_with_next.sh
+++ build_with_next.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export NEXTVERSION=$(curl --silent "https://api.github.com/repos/eclipsesource/jsonforms/releases" | grep '"tag_name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
+NEXTVERSION=$(curl --silent "https://api.github.com/repos/eclipsesource/jsonforms/releases" | grep '"tag_name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
+
+if [[ ${NEXTVERSION:0:1} == "v" ]] ; then NEXTVERSION="${NEXTVERSION:1}"; fi
 
 DOCZ_NEXTVERSION=$NEXTVERSION npm run build:docz
 
