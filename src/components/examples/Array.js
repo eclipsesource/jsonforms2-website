cf6f8e3301c0919b21eac1e78ca922939e49796b
Florian Gareis <mail@zoker.me>
2021-02-02 14:38:58 +0100

    Enhance array example

--- src/components/examples/Array.js
+++ src/components/examples/Array.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { array } from '@jsonforms/examples';
 import Demo from '../common/Demo';
 import styles from '../../styles/global.module.css';
 
 const Array = () => {
+  const theme = createMuiTheme({
+    overrides: {
+      MuiSelect: {
+        select: {
+          minWidth: 200,
+        }
+      }
+    }
+  });
+
   const uischema = {
     type: 'VerticalLayout',
     elements: [
@@ -16,16 +27,18 @@ const Array = () => {
 
   return (
     <div className={styles.example}>
-      <Demo
-        id='array-demo'
-        className={styles.examples__array}
-        schema={array.schema}
-        uischema={uischema}
-        data={array.data}
-        style={{
-          padding: 0,
-        }}
-      />
+      <ThemeProvider theme={theme}>
+        <Demo
+          id='array-demo'
+          className={styles.examples__array}
+          schema={array.schema}
+          uischema={uischema}
+          data={array.data}
+          style={{
+            padding: 0,
+          }}
+        />
+      </ThemeProvider>
       <div className={styles.examples__array_note}>
         Our current array renderer is not supported on mobile, sorry.
       </div>
