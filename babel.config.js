7f22a5d3ee6c2f8adb16539b6f935d440fa0ccc9
Stefan Dirix <sdirix@eclipsesource.com>
2021-05-06 15:54:35 +0200

    Revert "Update to docusaurus 2.0.0-alpha.75 and fix webpack issues"
    This reverts commit 9fd042167463dd308ed0ee8525050f56f3394208.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: [
-    ["@babel/plugin-proposal-private-methods", { "loose": false }],
-    "@babel/plugin-proposal-class-properties"
-  ]
+  plugins: ["@babel/plugin-proposal-class-properties"]
 };
