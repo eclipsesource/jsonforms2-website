9fd042167463dd308ed0ee8525050f56f3394208
Florian Gareis <mail@zoker.me>
2021-05-06 13:56:22 +0200

    Update to docusaurus 2.0.0-alpha.75 and fix webpack issues

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: ["@babel/plugin-proposal-class-properties"]
+  plugins: [
+    ["@babel/plugin-proposal-private-methods", { "loose": false }],
+    "@babel/plugin-proposal-class-properties"
+  ]
 };
