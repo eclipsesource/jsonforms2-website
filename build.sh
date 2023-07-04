fa6daa97577b494edc53eb9ad8a27b930d575b16
Florian Gareis <flo.gareis@tum.de>
2023-07-04 11:19:35 +0200

    Add default security headers

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# 
+# Add versions to frontpage
+#
+
 function next() {
     NEXTVERSIONCANDIDATE=$(curl --silent "https://api.github.com/repos/eclipsesource/jsonforms/tags" | grep '"name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
     if [[ ${NEXTVERSIONCANDIDATE:0:1} == "v" ]] && [[ $NEXTVERSIONCANDIDATE != $CURRENTVERSION ]] ; then NEXTVERSION="${NEXTVERSIONCANDIDATE:1}"; fi
@@ -20,3 +24,16 @@ if [[ $WITH_NEXT = "next" ]]; then
 else
     current
 fi
+
+# 
+# Add beacon hash to netlify.toml file
+#
+
+# Download the file
+curl -O https://static.cloudflareinsights.com/beacon.min.js
+
+# Calculate the sha256 hash of the file and convert it to base64
+hash=$(openssl dgst -sha256 -binary beacon.min.js | openssl base64)
+
+# Replace the placeholder in netlify.toml file with the calculated hash
+sed -i "s|##HASH_PLACEHOLDER##|'sha256-$hash'|g" netlify.toml
