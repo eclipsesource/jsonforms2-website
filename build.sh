51ec3d69b010f639bff9f717f587b8950b63e1fa
Florian Gareis <dev@zoker.me>
2023-09-14 12:24:55 +0200

    Remove build script

--- build.sh
+++ build.sh
@@ -24,16 +24,3 @@ if [[ $WITH_NEXT = "next" ]]; then
 else
     current
 fi
-
-# 
-# Add beacon hash to netlify.toml file
-#
-
-# Download the file
-curl -O https://static.cloudflareinsights.com/beacon.min.js
-
-# Calculate the sha256 hash of the file and convert it to base64
-hash=$(openssl dgst -sha256 -binary beacon.min.js | openssl base64)
-
-# Replace the placeholder in netlify.toml file with the calculated hash
-sed -i "s|##HASH_PLACEHOLDER##|'sha256-$hash'|g" netlify.toml
